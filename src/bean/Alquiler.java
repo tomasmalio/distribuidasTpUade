4d9a1d464fea7000a2e9c335539faa16a7657947
FlorOtero <otero.florencia96@gmail.com>
2018-03-20 10:00:13 -0300

    wip

--- src/bean/Alquiler.java
+++ src/bean/Alquiler.java
@@ -2,10 +2,19 @@ package bean;
 
 import java.util.Date;
 
-public class Alquiler {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+@Entity
+@Table(name="alquileres")
+public class Alquiler extends Servicio{
+	@Column (name = "fechaDesde", nullable = false, length = 50)
 	Date fechaDesde;
+	@Column (name = "fechaHasta", nullable = false, length = 50)
 	Date fechaHasta;
+	@Column (name = "comisionGestion", nullable = false, length = 50)
 	float comisionGestion;
+	@Column (name = "gastosSellado", nullable = false, length = 50)
 	float gastosSellado;
 	public Date getFechaDesde() {
 		return fechaDesde;

38bec477d139851305d84e032e81c6409969d912
tomasmalio <tomasmalio@gmail.com>
2018-03-20 16:46:11 -0300

    Modify alquiler list

--- src/bean/AlquilerList.java
+++ src/bean/AlquilerList.java
@@ -1,5 +1,22 @@
 package bean;
 
-public class AlquilerList {
+import java.util.ArrayList;
+import java.util.List;
 
+import bean.Alquiler;
+
+public class AlquilerList {
+	private List<Alquiler> alquileres = new ArrayList<Alquiler>();
+	
+	public List<Alquiler> getAlquileres() {
+		return alquileres;
+	}
+	
+	public void setAlquileres(List<Alquiler> alquileres) {
+		this.alquileres = alquileres;
+	}
+	
+	public boolean removeAlquiler (Alquiler alquiler) {
+		return alquileres.remove(alquiler);
+	}
 }

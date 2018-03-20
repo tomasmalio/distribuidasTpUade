13f7994d5d16f24b6dab7cdefd08125fa54c925f
tomasmalio <tomasmalio@gmail.com>
2018-03-20 12:10:12 -0300

    Add views

---
+++ src/views/AlquilerBaja.java
@@ -0,0 +1,20 @@
+package views;
+
+import app.SistemaInmobiliaria;
+
+public class AlquilerBaja extends javax.swing.JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	private SistemaInmobiliaria sistema;
+
+	public AlquilerBaja  (SistemaInmobiliaria s) {
+		super();
+		sistema = s;
+		initGui();
+	}
+	
+	private void initGui () {
+		
+	}
+}
---
+++ src/views/AlquilerModificar.java
@@ -0,0 +1,20 @@
+package views;
+
+import app.SistemaInmobiliaria;
+
+public class AlquilerModificar extends javax.swing.JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	private SistemaInmobiliaria sistema;
+
+	public AlquilerModificar  (SistemaInmobiliaria s) {
+		super();
+		sistema = s;
+		initGui();
+	}
+	
+	private void initGui () {
+		
+	}
+}

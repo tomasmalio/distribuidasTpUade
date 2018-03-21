780d44362703deaf5a7864fcac487d90b310662d
tomasmalio <tomasmalio@gmail.com>
2018-03-21 00:14:08 -0300

    UX Time Rocks

--- src/views/AlquilerAlta.java
+++ src/views/AlquilerAlta.java
@@ -64,51 +64,14 @@ public class AlquilerAlta {
 	private void initialize() {
 		
 		frmAlquilerAlta = new JFrame();
-		frmAlquilerAlta.setBounds(100, 100, 600, 600);
+		frmAlquilerAlta.setBounds(100, 100, 320, 600);
 		frmAlquilerAlta.setAlwaysOnTop(true);
 		frmAlquilerAlta.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmAlquilerAlta.getContentPane().setLayout(null);
 		
-		JLabel lblFechaDesde = new JLabel("Fecha Desde:");
-		lblFechaDesde.setBounds(6, 218, 85, 16);
-		frmAlquilerAlta.getContentPane().add(lblFechaDesde);
-		
-		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-		textFechaDesde = new JFormattedTextField(dateFormat);
-		textFechaDesde.setBounds(103, 213, 130, 26);
-		frmAlquilerAlta.getContentPane().add(textFechaDesde);
-		textFechaDesde.setColumns(10);
-		
-		JLabel lblFechaHasta = new JLabel("Fecha Hasta:");
-		lblFechaHasta.setBounds(6, 283, 85, 16);
-		frmAlquilerAlta.getContentPane().add(lblFechaHasta);
-		
-		textFechaHasta = new JFormattedTextField(dateFormat);
-		textFechaHasta.setBounds(103, 278, 130, 26);
-		frmAlquilerAlta.getContentPane().add(textFechaHasta);
-		textFechaHasta.setColumns(10);
-		
 		JLabel lblPropiedad = new JLabel("Propiedad:");
-		lblPropiedad.setBounds(213, 6, 80, 16);
+		lblPropiedad.setBounds(33, 6, 80, 16);
 		frmAlquilerAlta.getContentPane().add(lblPropiedad);
-		
-		JLabel lblComision = new JLabel("Comisión:");
-		lblComision.setBounds(245, 218, 90, 16);
-		frmAlquilerAlta.getContentPane().add(lblComision);
-		
-		JLabel lblGastoSellado = new JLabel("Gasto Sellado:");
-		lblGastoSellado.setBounds(245, 283, 90, 16);
-		frmAlquilerAlta.getContentPane().add(lblGastoSellado);
-		
-		textComision = new JTextField();
-		textComision.setBounds(358, 213, 130, 26);
-		frmAlquilerAlta.getContentPane().add(textComision);
-		textComision.setColumns(10);
-		
-		textSellado = new JTextField();
-		textSellado.setBounds(358, 278, 130, 26);
-		frmAlquilerAlta.getContentPane().add(textSellado);
-		textSellado.setColumns(10);
 		
 		String[] columnNames = {"Calle", "Propietario"};
 		tablePropiedades = new JTable();
@@ -126,12 +89,49 @@ public class AlquilerAlta {
 		tablePropiedades.getColumnModel().getColumn(1).setMaxWidth(100);
 
 		JScrollPane scrollPanePropiedades = new JScrollPane(tablePropiedades);
-		scrollPanePropiedades.setBounds(159, 60, 245, 133);
+		scrollPanePropiedades.setBounds(33, 35, 250, 150);
 		tablePropiedades.setFillsViewportHeight(true);
 		frmAlquilerAlta.getContentPane().add(scrollPanePropiedades);
 		
+		JLabel lblFechaDesde = new JLabel("Fecha Desde:");
+		lblFechaDesde.setBounds(33, 198, 85, 16);
+		frmAlquilerAlta.getContentPane().add(lblFechaDesde);
+		
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		textFechaDesde = new JFormattedTextField(dateFormat);
+		textFechaDesde.setBounds(33, 220, 110, 26);
+		frmAlquilerAlta.getContentPane().add(textFechaDesde);
+		textFechaDesde.setColumns(10);
+		
+		JLabel lblFechaHasta = new JLabel("Fecha Hasta:");
+		lblFechaHasta.setBounds(175, 198, 85, 16);
+		frmAlquilerAlta.getContentPane().add(lblFechaHasta);
+		
+		textFechaHasta = new JFormattedTextField(dateFormat);
+		textFechaHasta.setBounds(175, 220, 110, 26);
+		frmAlquilerAlta.getContentPane().add(textFechaHasta);
+		textFechaHasta.setColumns(10);
+		
+		JLabel lblComision = new JLabel("Comisión:");
+		lblComision.setBounds(33, 265, 90, 16);
+		frmAlquilerAlta.getContentPane().add(lblComision);
+		
+		textComision = new JTextField();
+		textComision.setBounds(150, 263, 135, 26);
+		frmAlquilerAlta.getContentPane().add(textComision);
+		textComision.setColumns(10);
+		
+		JLabel lblGastoSellado = new JLabel("Gasto Sellado:");
+		lblGastoSellado.setBounds(33, 305, 90, 16);
+		frmAlquilerAlta.getContentPane().add(lblGastoSellado);
+		
+		textSellado = new JTextField();
+		textSellado.setBounds(150, 300, 135, 26);
+		frmAlquilerAlta.getContentPane().add(textSellado);
+		textSellado.setColumns(10);
+		
 		JLabel lblPersona = new JLabel("Persona");
-		lblPersona.setBounds(213, 332, 61, 16);
+		lblPersona.setBounds(33, 340, 61, 16);
 		frmAlquilerAlta.getContentPane().add(lblPersona);
 		
 		String[] columnNamesPers = {"Nombre", "CUIT"};
@@ -149,7 +149,7 @@ public class AlquilerAlta {
 		tablePersonas.getColumnModel().getColumn(1).setMaxWidth(100);
 
 		JScrollPane scrollPanePersonas = new JScrollPane(tablePersonas);
-		scrollPanePersonas.setBounds(159, 374, 209, 149);
+		scrollPanePersonas.setBounds(33, 364, 250, 149);
 		tablePersonas.setFillsViewportHeight(true);
 		frmAlquilerAlta.getContentPane().add(scrollPanePersonas);
 		
@@ -161,7 +161,7 @@ public class AlquilerAlta {
 			}
 		});
 		
-		btnAlquilar.setBounds(433, 549, 117, 29);
+		btnAlquilar.setBounds(175, 530, 117, 29);
 		frmAlquilerAlta.getContentPane().add(btnAlquilar);
 	}
 	

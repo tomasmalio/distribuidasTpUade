76ee6ab80fe530c198ed90a9ac2b479871a66c00
tomasmalio <tomasmalio@gmail.com>
2018-03-20 22:56:12 -0300

    Setup

--- src/app/Inicio.java
+++ src/app/Inicio.java
@@ -53,43 +53,13 @@ public class Inicio extends JFrame {
 	public Inicio() {
 		super();
 		inmobiliariaController = SistemaInmobiliaria.getInstance();
-		setup();
+		Setup setup = new Setup();
 		initGUI();
 	}
 	
     public static void main(String[] args) {
 	    	Inicio inicio = new Inicio();
 	    	inicio.setVisible(true);
-    }
-    
-    private void setup() {
-    	/**
-    	 * Carga de datos de Personas
-    	 */
-		inmobiliariaController.addPersona("12348", "Tomás", "Lima 123", "1158259693", "tomasmalio@gmail.com" );
-		inmobiliariaController.grabarPersonas(inmobiliariaController.getPersonas());
-		
-		
-	/**
-    	 * Carga de datos de Propiedades
-    	 */
-		inmobiliariaController.addPersona("12345", "Martín", "Av. Cabildo 1000", "1150001000", "martin.sacco@gmail.com");
-		inmobiliariaController.addPersona("12346", "Juan", "Av. Monroe 4000", "1520004444", "juan.pedro@gmail.com");
-		inmobiliariaController.addPropiedad("1", "Av. Cabildo", 3530, "3", "A", "Buenos Aires", "3", 120, 12000, 1000000, 100, 50, 500, inmobiliariaController.buscarPersona("12345"));
-		inmobiliariaController.addPropiedad("2", "Juramento", 2060, "15", "B", "Buenos Aires", "5", 320, 26000, 2000000, 2030, 150, 1500, inmobiliariaController.buscarPersona("12345"));
-		inmobiliariaController.addPropiedad("3", "Av. Lacroze", 460, "5", "D", "Buenos Aires", "1", 90, 8000, 750000, 80, 60, 300, inmobiliariaController.buscarPersona("12346"));
-		inmobiliariaController.grabarPropiedades(inmobiliariaController.getPropiedades());
-		
-		try {
-			Date fechaRegServicio 	= new SimpleDateFormat("yyyy-MM-dd").parse("2017-12-10");
-			Date fechaDesde 			= new SimpleDateFormat("yyyy-MM-dd").parse("2018-01-01");
-			Date fechaHasta 			= new SimpleDateFormat("yyyy-MM-dd").parse("2019-01-01");
-			
-			inmobiliariaController.addAlquiler(fechaDesde, fechaHasta, 1550, 3000, fechaRegServicio, inmobiliariaController.buscarPropiedad("1"), inmobiliariaController.buscarPersona("12346"));
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		
     }
    
     private void initGUI() {
